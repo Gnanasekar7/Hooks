b7c8c96a90a1ee6d3609f0e08f1a6cb0b909df2c
Gnanasekar K <gnanasekar.k@optisolbusiness.com>
2023-03-17 15:36:07 +0530

    committ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+// import HooksCounterThree from './components/HooksCounterThree';
+// import Hookcounter from './components/HookCounter';
+// import ClassCount from './components/ClassCount';
+// import HookcounterTwo from './components/HookcounterTwo';
+// import HookCounterFour from './components/HookCounterFour';
+// import EffectClass from './components/EffectClass';
+// import Effect1 from './components/Effect1';
+// import ClassMouse from './components/ClassMouse';
+// import HookMouse from './components/HookMouse';
+// import MouseContainer from './components/MouseContainer';
+// import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ClassCount/> */}
+     {/* <Hookcounter/> */}
+     {/* <HookcounterTwo/> */}
+     {/* <HooksCounterThree/> */}
+     {/* <HookCounterFour/> */}
+     {/* <EffectClass/> */}
+     {/* <Effect1 /> */}
+     {/* <ClassMouse/> */}
+     {/* <HookMouse/> */}
+     {/* <MouseContainer/> */}
+     {/* <IntervalClassCounter/> */}
+     <IntervalHookCounter/>
     </div>
   );
 }
